9b9461696c735dba5795e70f759baa722a449969
are <repkin572@gmail.com>
2021-12-24 00:40:46 +0700

    working on tests in testcontainer

---
+++ src/test/java/com/library/graphqltest/TestContainerConfig.java
@@ -0,0 +1,31 @@
+package com.library.graphqltest;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public class TestContainerConfig extends PostgreSQLContainer<TestContainerConfig> {
+    private static final String IMAGE_VERSION = "postgres:11.1";
+    private static TestContainerConfig container;
+
+    private TestContainerConfig() {
+        super(IMAGE_VERSION);
+    }
+
+    public static TestContainerConfig getInstance() {
+        if (container == null) {
+            container = new TestContainerConfig();
+        }
+        return container;
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        System.setProperty("DB_URL", container.getJdbcUrl());
+        System.setProperty("DB_USERNAME", container.getUsername());
+        System.setProperty("DB_PASSWORD", container.getPassword());
+    }
+
+    @Override
+    public void stop() {
+    }
+}
--- src/test/java/com/library/graphqltest/resolver/author/AuthorQueryResolverTest.java
+++ src/test/java/com/library/graphqltest/resolver/author/AuthorQueryResolverTest.java
@@ -1,46 +1,52 @@
 package com.library.graphqltest.resolver.author;
 
-import com.library.graphqltest.TestContainersContainerConfig.PostgresContainer;
-import com.library.graphqltest.entity.Author;
-import com.library.graphqltest.repository.AuthorRepository;
+import com.graphql.spring.boot.test.GraphQLResponse;
+import com.graphql.spring.boot.test.GraphQLTestTemplate;
+import com.library.graphqltest.GraphqlTestApplication;
+import com.library.graphqltest.TestContainerConfig;
+import org.json.JSONException;
 import org.junit.ClassRule;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import javax.transaction.Transactional;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.text.MessageFormat;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@Testcontainers
+@ActiveProfiles("test")
+@SpringBootTest(classes = GraphqlTestApplication.class)
 class AuthorQueryResolverIntegrationTest {
   @Autowired
-  private AuthorRepository authorRepository;
+  GraphQLTestTemplate graphQLTestTemplate;
 
-  @BeforeEach
-  void setUp() {
-  }
+  @ClassRule
+  public static PostgreSQLContainer<TestContainerConfig> postgreSQLContainer = TestContainerConfig.getInstance();
 
-  @AfterEach
-  void tearDown() {
-  }
-
-  @Container
-  public static PostgresContainer postgresContainer = PostgresContainer.getInstance();
+  private static final String GRAPHQL_QUERY_REQUEST_PATH = "graphql/resolver/query/{}.graphqls";
+  private static final String GRAPHQL_QUERY_RESPONSE_PATH = "graphql/resolver/query/{}.json";
 
   @Test
-  void getAuthor() {
-    Author author = new Author();
-    author.setName("TestAuthor");
-    Author authorSaved = authorRepository.save(author);
-    Assertions.assertEquals(author.getName(), authorSaved.getName());
+  @Transactional
+  void getAuthor() throws IOException, JSONException {
+    String testName = "getAuthor";
+    GraphQLResponse graphQLResponse = graphQLTestTemplate.postForResource(MessageFormat.format(GRAPHQL_QUERY_REQUEST_PATH, testName));
+    String expectedResponseBody = readFile(MessageFormat.format(GRAPHQL_QUERY_RESPONSE_PATH, testName));
+    Assertions.assertTrue(graphQLResponse.isOk());
+    JSONAssert.assertEquals(expectedResponseBody, graphQLResponse.getRawResponse().getBody(), true);
+  }
+
+  private String readFile(String location) throws IOException {
+    return new String(new ClassPathResource(location).getInputStream().readAllBytes(), StandardCharsets.UTF_8);
   }
 }
